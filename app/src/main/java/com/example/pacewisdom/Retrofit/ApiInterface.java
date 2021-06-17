4fb1289ad3036f3beb8305a954efc163170dbaf2
Amit Kumar <er.amitsharma957@gmail.com>
2021-06-17 23:04:04 +0530

    hello word

--- app/src/main/java/com/example/pacewisdom/Retrofit/ApiInterface.java
+++ app/src/main/java/com/example/pacewisdom/Retrofit/ApiInterface.java
@@ -23,43 +23,11 @@ public interface ApiInterface {
 
  @FormUrlEncoded
  @POST("")
- public Call<JsonElement> SaveGallery(@Field("/16k65ZRoYEFVqn7__cfvxrSC3FMB5517d") String File_Base64);
-
-
-
- /*@GET("login/user")
- public Call<List<LoginResult>> Login(@QueryMap Map<String, String> options);
-
- @GET("class/get")
- public Call<JsonElement> getClass(@QueryMap Map<String, String> options);
- @GET("section/get")
- public Call<JsonElement> Section(@QueryMap Map<String, String> options);
-
- @GET("subject/get")
- public Call<JsonElement> getsubject();
-
- @GET("assignment/get")
- public Call<List<Assignments>> getassignmentlist(@QueryMap Map<String, String> options);
-
- @GET("notification/GetByID")
- public Call<List<Announcement>> getannouncementlist(@QueryMap Map<String, String> options);
-
- @GET("gallery/GetByID")
- public Call<List<GalleryClass>> getgallerylist(@QueryMap Map<String, String> options);
+ public Call<JsonElement> SaveGallery(@Field("/16k65ZRoYEFVqn7__cfvxrSC3FMB5517d") String Filetest);
 
 
 
 
- @POST("assignment/Delete")
- public Call<String> deleteAssignment(@QueryMap Map<String, String> options);
-
- @POST("notification/Delete")
- public Call<String> deleteAnnouncement(@QueryMap Map<String, String> options);
-
- @POST("gallery/Delete")
- public Call<String> deleteGallery(@QueryMap Map<String, String> options);
-
-*/
 
 
 }
